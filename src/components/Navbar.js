d434079662f45d21fbdded0a7fa67722d542a5fb
jck-bit <jckkinyanjui@gmail.com>
2022-03-05 19:55:18 +0300

    two contexts in one single component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
+import { AuthContext } from '../context/AuthContext';
 import { ThemeContext } from '../context/ThemeContext';
 
 class Navbar extends Component {
   render() {
     return ( 
-        <ThemeContext.Consumer>{(context) =>{
-            const { isLightTheme, light, dark } = context;
-            const theme = isLightTheme ? light : dark;
-            return(
-             <nav style={{ background: theme.ui, color: theme.syntax }}>
-                <h1>Context App</h1>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-             </nav>
-            )
-        }}
-        </ThemeContext.Consumer>
+        <AuthContext.Consumer>{(authcontext) =>(
+            <ThemeContext.Consumer>{(themecontext) =>{
+                const {isAuthenticated, toggleAuth} = authcontext;
+                const { isLightTheme, light, dark } = themecontext;
+                const theme = isLightTheme ? light : dark;
+
+                return(
+                 <nav style={{ background: theme.ui, color: theme.syntax }}>
+                    <h1>Context App</h1>
+                    <div onClick={toggleAuth}>
+                        {isAuthenticated? 'Logged in' : 'Logged out'}
+                    </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                 </nav>
+                )
+            }}
+            </ThemeContext.Consumer>
+        )}
+      
+        </AuthContext.Consumer>
    
     );
   }
