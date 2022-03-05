e8ceaa3f91bb508f228c5e33d84e9967a4c0e864
jck-bit <jckkinyanjui@gmail.com>
2022-03-05 19:35:12 +0300

    multiple contexts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Booklist from './components/Booklist';
 import Navbar from './components/Navbar';
 import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './context/AuthContext';
 import ThemeContextProvider from './context/ThemeContext';
 
 function App() {
   return ( 
     <div className="App">
      <ThemeContextProvider>
+      <AuthContextProvider>
         <Navbar />
         <Booklist/>
         <ThemeToggle/>
+      </AuthContextProvider>
      </ThemeContextProvider>
     </div>
   );
